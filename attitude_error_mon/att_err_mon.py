1159024d52e7a058e00e0694a12da6dcd7e93853
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-05-28 11:43:01 -0400

    Formatting fixes

--- attitude_error_mon/att_err_mon.py
+++ attitude_error_mon/att_err_mon.py
@@ -5,8 +5,9 @@ import argparse
 import jinja2
 import numpy as np
 import matplotlib
-if __name__ == '__main__':
-    matplotlib.use('Agg')
+
+if __name__ == "__main__":
+    matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 from astropy.table import Table, vstack, Column
@@ -22,15 +23,14 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_options():
-    parser = argparse.ArgumentParser(
-        description="Update attitude error plots")
-    parser.add_argument("--outdir",
-                        default=".")
-    parser.add_argument("--datadir",
-                        default=".")
-    parser.add_argument("--recent-start",
-                        help="Date to use as reference for highlighted/recent data," +
-                        " default is to highlight last 60 days")
+    parser = argparse.ArgumentParser(description="Update attitude error plots")
+    parser.add_argument("--outdir", default=".")
+    parser.add_argument("--datadir", default=".")
+    parser.add_argument(
+        "--recent-start",
+        help="Date to use as reference for highlighted/recent data,"
+        + " default is to highlight last 60 days",
+    )
 
     opt = parser.parse_args()
     return opt
@@ -57,31 +57,35 @@ def get_obs_table(start, stop):
             obsid = 0
 
         if last_npnt_stop is not None and m.npnt_start is not None:
-            obs['nmm_time'] = CxoTime(m.npnt_start).secs - CxoTime(last_npnt_stop).secs
+            obs["nmm_time"] = CxoTime(m.npnt_start).secs - CxoTime(last_npnt_stop).secs
         else:
-            obs['nmm_time'] = 0
+            obs["nmm_time"] = 0
         if m.npnt_stop is not None:
             last_npnt_stop = m.npnt_stop
 
-        if m.npnt_start is None or obs['nmm_time'] == 0:
+        if m.npnt_start is None or obs["nmm_time"] == 0:
             continue
 
-        obs['obsid'] = obsid
-        obs['date'] = m.start
-        obs['time'] = DateTime(m.start).secs
-        obs['manvr_angle'] = m.angle
-        obs['one_shot'] = m.one_shot
-        obs['one_shot_yaw'] = m.one_shot_yaw
-        obs['one_shot_pitch'] = m.one_shot_pitch
-        obs['dwell_duration'] = DateTime(m.next_nman_start).secs - DateTime(m.npnt_start).secs
+        obs["obsid"] = obsid
+        obs["date"] = m.start
+        obs["time"] = DateTime(m.start).secs
+        obs["manvr_angle"] = m.angle
+        obs["one_shot"] = m.one_shot
+        obs["one_shot_yaw"] = m.one_shot_yaw
+        obs["one_shot_pitch"] = m.one_shot_pitch
+        obs["dwell_duration"] = (
+            DateTime(m.next_nman_start).secs - DateTime(m.npnt_start).secs
+        )
 
         try:
             all_err = {}
-            for err_name, err_msid in zip(['roll_err', 'pitch_err', 'yaw_err'],
-                                          ['AOATTER1', 'AOATTER2', 'AOATTER3']):
-                err = fetch.Msid(err_msid,
-                                 DateTime(m.npnt_start).secs + 500,
-                                 m.next_nman_start)
+            for err_name, err_msid in zip(
+                ["roll_err", "pitch_err", "yaw_err"],
+                ["AOATTER1", "AOATTER2", "AOATTER3"],
+            ):
+                err = fetch.Msid(
+                    err_msid, DateTime(m.npnt_start).secs + 500, m.next_nman_start
+                )
                 if len(err.times):
                     events.dumps.interval_pad = (0, 300)
                     err.remove_intervals(events.dumps)
@@ -92,182 +96,238 @@ def get_obs_table(start, stop):
 
             # If there are no samples left, that is not an error condition, and the
             # attitude errors should just be counted as 0.
-            if len(all_err['roll_err'].vals) == 0:
-                obs['roll_err'] = 0
-                obs['point_err'] = 0
+            if len(all_err["roll_err"].vals) == 0:
+                obs["roll_err"] = 0
+                obs["point_err"] = 0
             else:
-                obs['roll_err'] = np.degrees(
-                    np.percentile(np.abs(all_err['roll_err'].vals), 99)) * 3600
-                point_err = np.sqrt((all_err['pitch_err'].vals ** 2) +
-                                    (all_err['yaw_err'].vals ** 2))
-                obs['point_err'] = np.degrees(np.percentile(point_err, 99)) * 3600
+                obs["roll_err"] = (
+                    np.degrees(np.percentile(np.abs(all_err["roll_err"].vals), 99))
+                    * 3600
+                )
+                point_err = np.sqrt(
+                    (all_err["pitch_err"].vals ** 2) + (all_err["yaw_err"].vals ** 2)
+                )
+                obs["point_err"] = np.degrees(np.percentile(point_err, 99)) * 3600
 
         # If there are issues indexing into the AOATTER data, that's an error condition
         # that should just be captured by a large/bogus value.
         except IndexError:
-            obs['point_err'] = 999
-            obs['roll_err'] = 999
+            obs["point_err"] = 999
+            obs["roll_err"] = 999
         obs_data.append(obs)
 
     return Table(obs_data)
 
 
-def one_shot_plot(ref_data, recent_data, outdir='.'):
-
+def one_shot_plot(ref_data, recent_data, outdir="."):
     # Grab the date of the start and stop of the two data sets.
-    d0_str = ref_data['date'][0][0:8]
-    d1_str = ref_data['date'][-1][0:8]
-    d2_str = recent_data['date'][0][0:8]
-    d3_str = recent_data['date'][-1][0:8]
+    d0_str = ref_data["date"][0][0:8]
+    d1_str = ref_data["date"][-1][0:8]
+    d2_str = recent_data["date"][0][0:8]
+    d3_str = recent_data["date"][-1][0:8]
 
     plt.figure(figsize=(7, 4))
-    plt.plot(ref_data['manvr_angle'], ref_data['one_shot'], 'b+',
-             markersize=5, markeredgewidth=1.0, alpha=.25, label=f'{d0_str} to {d1_str}')
-    plt.plot(recent_data['manvr_angle'], recent_data['one_shot'], 'rx',
-             markersize=5, markeredgewidth=.8, label=f'{d2_str} to {d3_str}')
+    plt.plot(
+        ref_data["manvr_angle"],
+        ref_data["one_shot"],
+        "b+",
+        markersize=5,
+        markeredgewidth=1.0,
+        alpha=0.25,
+        label=f"{d0_str} to {d1_str}",
+    )
+    plt.plot(
+        recent_data["manvr_angle"],
+        recent_data["one_shot"],
+        "rx",
+        markersize=5,
+        markeredgewidth=0.8,
+        label=f"{d2_str} to {d3_str}",
+    )
     plt.grid()
     plt.xlim(0, 185)
     plt.ylim(ymin=0)
-    plt.ylabel('One Shot (arcsec)')
-    plt.xlabel('Manvr Angle (deg)')
-    plt.title('One Shot Magnitude', fontsize=12, y=1.05)
-    plt.legend(loc='upper left', fontsize=8)
+    plt.ylabel("One Shot (arcsec)")
+    plt.xlabel("Manvr Angle (deg)")
+    plt.title("One Shot Magnitude", fontsize=12, y=1.05)
+    plt.legend(loc="upper left", fontsize=8)
     plt.tight_layout()
-    plt.savefig(os.path.join(outdir, 'one_shot_vs_angle.png'))
+    plt.savefig(os.path.join(outdir, "one_shot_vs_angle.png"))
 
     plt.figure(figsize=(7, 4))
-    plt.plot(ref_data['nmm_time'], ref_data['one_shot'], 'b+',
-             markersize=5, markeredgewidth=1.0, alpha=.25, label=f'{d0_str} to {d1_str}')
-    plt.plot(recent_data['nmm_time'], recent_data['one_shot'], 'rx',
-             markersize=5, markeredgewidth=.8, label=f'{d2_str} to {d3_str}')
+    plt.plot(
+        ref_data["nmm_time"],
+        ref_data["one_shot"],
+        "b+",
+        markersize=5,
+        markeredgewidth=1.0,
+        alpha=0.25,
+        label=f"{d0_str} to {d1_str}",
+    )
+    plt.plot(
+        recent_data["nmm_time"],
+        recent_data["one_shot"],
+        "rx",
+        markersize=5,
+        markeredgewidth=0.8,
+        label=f"{d2_str} to {d3_str}",
+    )
     plt.grid()
     plt.xlim(0, 4000)
     plt.ylim(ymin=0)
-    plt.ylabel('One Shot (arcsec)')
-    plt.xlabel('Time in NMM (s)')
-    plt.title('One Shot Magnitude', fontsize=12, y=1.05)
-    plt.legend(loc='upper left', fontsize=8)
+    plt.ylabel("One Shot (arcsec)")
+    plt.xlabel("Time in NMM (s)")
+    plt.title("One Shot Magnitude", fontsize=12, y=1.05)
+    plt.legend(loc="upper left", fontsize=8)
     plt.tight_layout()
-    plt.savefig(os.path.join(outdir, 'one_shot_vs_nmmtime.png'))
+    plt.savefig(os.path.join(outdir, "one_shot_vs_nmmtime.png"))
 
 
-def att_err_time_plots(ref_data, recent_data, min_dwell_time=1000, outdir='.'):
-    ref_data = ref_data[ref_data['dwell_duration'] >= min_dwell_time]
-    recent_data = recent_data[recent_data['dwell_duration'] >= min_dwell_time]
+def att_err_time_plots(ref_data, recent_data, min_dwell_time=1000, outdir="."):
+    ref_data = ref_data[ref_data["dwell_duration"] >= min_dwell_time]
+    recent_data = recent_data[recent_data["dwell_duration"] >= min_dwell_time]
 
     # Grab the date of the start and stop of the two data sets.
-    d0_str = ref_data['date'][0][0:8]
-    d1_str = ref_data['date'][-1][0:8]
-    d2_str = recent_data['date'][0][0:8]
-    d3_str = recent_data['date'][-1][0:8]
+    d0_str = ref_data["date"][0][0:8]
+    d1_str = ref_data["date"][-1][0:8]
+    d2_str = recent_data["date"][0][0:8]
+    d3_str = recent_data["date"][-1][0:8]
 
-    for i, ax, msid in zip([1, 2], ['roll', 'point'], ['abs(aoatter1)', 'rss(aoatter2,aoatter3)']):
+    for i, ax, msid in zip(
+        [1, 2], ["roll", "point"], ["abs(aoatter1)", "rss(aoatter2,aoatter3)"]
+    ):
         default_ylims = [0, 1.4]
-        if ax == 'roll':
+        if ax == "roll":
             default_ylims = [0, 12]
-        fig = plt.figure(figsize=(5, 3.5))
-        plot_cxctime(ref_data['time'], ref_data[f'{ax}_err'], 'b+', markersize=5,
-                     alpha=.5, label=f'{d0_str} to {d1_str}')
-        plot_cxctime(recent_data['time'], recent_data[f'{ax}_err'], 'rx', markersize=5,
-                     alpha=.5, label=f'{d2_str} to {d3_str}')
-        plt.margins(x=.05, y=.05)
-        plt.ylabel(f'{ax} err (arcsec)', fontsize=9)
-        plt.title(f'per obs 99th percentile {ax} err\n ({msid})',
-                  fontsize=12)
+        plt.figure(figsize=(5, 3.5))
+        plot_cxctime(
+            ref_data["time"],
+            ref_data[f"{ax}_err"],
+            "b+",
+            markersize=5,
+            alpha=0.5,
+            label=f"{d0_str} to {d1_str}",
+        )
+        plot_cxctime(
+            recent_data["time"],
+            recent_data[f"{ax}_err"],
+            "rx",
+            markersize=5,
+            alpha=0.5,
+            label=f"{d2_str} to {d3_str}",
+        )
+        plt.margins(x=0.05, y=0.05)
+        plt.ylabel(f"{ax} err (arcsec)", fontsize=9)
+        plt.title(f"per obs 99th percentile {ax} err\n ({msid})", fontsize=12)
         plt.grid()
         ylims = plt.ylim()
-        setlims = plt.ylim(np.min([ylims[0], default_ylims[0]]),
-                           np.max([ylims[1], default_ylims[1]]))
+        setlims = plt.ylim(
+            np.min([ylims[0], default_ylims[0]]), np.max([ylims[1], default_ylims[1]])
+        )
         plt.ylim(setlims)
         plt.xticks(fontsize=7)
         plt.tight_layout()
-        plt.savefig(os.path.join(outdir, f'{ax}_err_vs_time.png'))
+        plt.savefig(os.path.join(outdir, f"{ax}_err_vs_time.png"))
 
 
-def att_err_hist(ref_data, recent_data, label=None, min_dwell_time=1000, outdir='.'):
-    ref_data = ref_data[ref_data['dwell_duration'] >= min_dwell_time]
-    recent_data = recent_data[recent_data['dwell_duration'] >= min_dwell_time]
+def att_err_hist(ref_data, recent_data, label=None, min_dwell_time=1000, outdir="."):
+    ref_data = ref_data[ref_data["dwell_duration"] >= min_dwell_time]
+    recent_data = recent_data[recent_data["dwell_duration"] >= min_dwell_time]
 
     # Grab the date of the start and stop of the two data sets.
-    d0_str = ref_data['date'][0][0:8]
-    d1_str = ref_data['date'][-1][0:8]
-    d2_str = recent_data['date'][0][0:8]
-    d3_str = recent_data['date'][-1][0:8]
+    d0_str = ref_data["date"][0][0:8]
+    d1_str = ref_data["date"][-1][0:8]
+    d2_str = recent_data["date"][0][0:8]
+    d3_str = recent_data["date"][-1][0:8]
 
-    for i, ax, msid in zip([1, 2], ['roll', 'point'], ['abs(aoatter1)', 'rss(aoatter2, aoatter3)']):
+    for i, ax, msid in zip(
+        [1, 2], ["roll", "point"], ["abs(aoatter1)", "rss(aoatter2, aoatter3)"]
+    ):
         plt.figure(figsize=(5, 3.5))
-        if ax == 'roll':
-            bin_width = .25
+        if ax == "roll":
+            bin_width = 0.25
             lim = 15
         else:
-            bin_width = .05
+            bin_width = 0.05
             lim = 7.5
         bins = np.arange(0, lim + bin_width, bin_width)
-        plt.hist(ref_data[f'{ax}_err'], bins=bins, log=True, density=True, color='b',
-                 alpha=.4, label=f'{d0_str} to {d1_str}')
-        plt.hist(recent_data[f'{ax}_err'], bins=bins, log=True, density=True, color='r',
-                 alpha=.4, label=f'{d2_str} to {d3_str}')
-        plt.xlabel(f'{ax} err (arcsec)')
-        plt.legend(loc='upper right', fontsize=7)
-        plt.title(f'per obs 99th percentile {ax} err\n({msid})',
-                  fontsize=12)
+        plt.hist(
+            ref_data[f"{ax}_err"],
+            bins=bins,
+            log=True,
+            density=True,
+            color="b",
+            alpha=0.4,
+            label=f"{d0_str} to {d1_str}",
+        )
+        plt.hist(
+            recent_data[f"{ax}_err"],
+            bins=bins,
+            log=True,
+            density=True,
+            color="r",
+            alpha=0.4,
+            label=f"{d2_str} to {d3_str}",
+        )
+        plt.xlabel(f"{ax} err (arcsec)")
+        plt.legend(loc="upper right", fontsize=7)
+        plt.title(f"per obs 99th percentile {ax} err\n({msid})", fontsize=12)
         plt.grid()
         plt.tight_layout()
-        plt.savefig(os.path.join(outdir, f'{ax}_err_hist.png'))
+        plt.savefig(os.path.join(outdir, f"{ax}_err_hist.png"))
 
 
 def update_file_data(data_file, start, stop):
     if os.path.exists(data_file):
         print(f"Reading previous data from {data_file}")
-        last_data = Table.read(data_file, format='ascii')
-        new_data = get_obs_table(last_data[-5]['date'], stop)
-        if new_data['date'][0] > last_data['date'][-1]:
+        last_data = Table.read(data_file, format="ascii")
+        new_data = get_obs_table(last_data[-5]["date"], stop)
+        if new_data["date"][0] > last_data["date"][-1]:
             data = vstack([last_data, new_data])
         else:
-            idx_old_data = np.flatnonzero(last_data['date'] >= new_data['date'][0])[0]
+            idx_old_data = np.flatnonzero(last_data["date"] >= new_data["date"][0])[0]
             data = vstack([last_data[0:idx_old_data], new_data])
-        data = data[data['date'] >= start.date]
+        data = data[data["date"] >= start.date]
     else:
         data = get_obs_table(start, stop)
-    data.sort('date')
-    data.write(data_file, format='ascii', overwrite=True)
+    data.sort("date")
+    data.write(data_file, format="ascii", overwrite=True)
     return data
 
 
-def update(datadir, outdir, full_start, recent_start,
-           point_lim=7.5, roll_lim=15):
-
-    data_file = os.path.join(datadir, 'data.dat')
+def update(datadir, outdir, full_start, recent_start, point_lim=7.5, roll_lim=15):
+    data_file = os.path.join(datadir, "data.dat")
     dat = update_file_data(data_file, full_start, DateTime())
-    recent_data = dat[dat['time'] >= recent_start.secs]
-    ref_data = dat[dat['time'] < recent_start.secs]
+    recent_data = dat[dat["time"] >= recent_start.secs]
+    ref_data = dat[dat["time"] < recent_start.secs]
 
     one_shot_plot(ref_data, recent_data, outdir=outdir)
 
-    comments_file = os.path.join(outdir, 'comments.dat')
+    comments_file = os.path.join(outdir, "comments.dat")
     if os.path.exists(comments_file):
         comments = ascii.read(comments_file, Reader=ascii.FixedWidthTwoLine)
         # convert to a dict
-        comments = {comment['obsid']: comment['comment'] for comment in comments}
+        comments = {comment["obsid"]: comment["comment"] for comment in comments}
     else:
         comments = {}
 
-    ok = (recent_data['point_err'] < point_lim) | (recent_data['roll_err'] < roll_lim)
+    ok = (recent_data["point_err"] < point_lim) | (recent_data["roll_err"] < roll_lim)
     outliers = recent_data[~ok]
-    comments_col = Column(data=[comments.get(row['obsid'], '') for row in outliers],
-                          name='comment')
+    comments_col = Column(
+        data=[comments.get(row["obsid"], "") for row in outliers], name="comment"
+    )
     outliers.add_column(comments_col)
     recent_data = recent_data[ok]
 
-    ok = (ref_data['point_err'] < point_lim) | (ref_data['roll_err'] < roll_lim)
+    ok = (ref_data["point_err"] < point_lim) | (ref_data["roll_err"] < roll_lim)
     ref_data = ref_data[ok]
     att_err_time_plots(ref_data, recent_data, outdir=outdir)
     att_err_hist(ref_data, recent_data, outdir=outdir)
 
-    template_html = open(os.path.join(FILE_DIR, 'data', 'index_template.html')).read()
+    template_html = open(os.path.join(FILE_DIR, "data", "index_template.html")).read()
     template = jinja2.Template(template_html)
-    out_html = template.render(outliers=outliers, one_shot_start=ref_data['date'][0])
-    with open(os.path.join(outdir, 'index.html'), 'w') as fh:
+    out_html = template.render(outliers=outliers, one_shot_start=ref_data["date"][0])
+    with open(os.path.join(outdir, "index.html"), "w") as fh:
         fh.write(out_html)
 
 
@@ -282,9 +342,13 @@ def main(args=None):
     else:
         recent_start = DateTime(opt.recent_start)
 
-    update(outdir=opt.outdir, datadir=opt.datadir,
-           full_start=recent_start - 365, recent_start=recent_start)
+    update(
+        outdir=opt.outdir,
+        datadir=opt.datadir,
+        full_start=recent_start - 365,
+        recent_start=recent_start,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@
 from setuptools import setup
 
 entry_points = {
-        "console_scripts": [
-                    "attitude_error_mon=attitude_error_mon.att_err_mon:main",
-                ]
-    }
+    "console_scripts": [
+        "attitude_error_mon=attitude_error_mon.att_err_mon:main",
+    ]
+}
 
 setup(
-        name="attitude_error_mon",
-        author="Jean Connelly",
-        description="Attitude error monitoring and trending",
-        author_email="jconnelly@cfa.harvard.edu",
-        use_scm_version=True,
-        setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
-        zip_safe=False,
-        license=("New BSD/3-clause BSD License\nCopyright (c) 2019"
-               " Smithsonian Astrophysical Observatory\nAll rights reserved."),
-        entry_points=entry_points,
-        packages=["attitude_error_mon"],
-        package_data={
-                    "attitude_error_mon": ["data/index_template.html", "task_schedule.cfg"]
-                },
-    )
+    name="attitude_error_mon",
+    author="Jean Connelly",
+    description="Attitude error monitoring and trending",
+    author_email="jconnelly@cfa.harvard.edu",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
+    zip_safe=False,
+    license=(
+        "New BSD/3-clause BSD License\nCopyright (c) 2019"
+        " Smithsonian Astrophysical Observatory\nAll rights reserved."
+    ),
+    entry_points=entry_points,
+    packages=["attitude_error_mon"],
+    package_data={
+        "attitude_error_mon": ["data/index_template.html", "task_schedule.cfg"]
+    },
+)
