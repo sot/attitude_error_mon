cbf6469a869ce9b43e066d18cadb18cb8ec2d1fd
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-08-01 10:23:04 -0400

    Update init doc

--- attitude_error_mon/__init__.py
+++ attitude_error_mon/__init__.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Ska_helpers is a collection of utilities for the Ska3 runtime environment.
+attitude_error_mon is a package supporting trending of attitude errors
+including one shot data.
 """
 from ska_helpers.version import get_version
 
